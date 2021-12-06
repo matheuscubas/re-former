923bb283753150615462062853377556cc56861d
Ryuukhi <maatheusviidal@hotmail.com>
2021-12-05 23:15:17 -0300

    finished edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password)
+  end
 
   def create
     def user_params
@@ -14,6 +18,28 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    def user_params
+      params.require(:user).permit(:name,:email,:password)
+    end
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'users#new'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update, :show]
 end
